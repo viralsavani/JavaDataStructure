873206da05a6ecff9c4b24d56bd6554b949c5481
Viral Savani <viralsavani@gmail.com>
2015-01-31 19:08:50 -0800

    Implemented static methods in StackTraingle

--- src/com/Recursion/StackTriangle.java
+++ src/com/Recursion/StackTriangle.java
@@ -36,9 +36,72 @@ class StackX{
 
 public class StackTriangle {
 
+	static int theAnswer;
+	static int theNumber;
+	static int codePart;
+	static Params theseParams;
+	static StackX theStack;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		theNumber = 100;		//Main input here.
+		recTriangle();
+		
+		System.out.println("Triangle :: " + theAnswer);
+	}
+	
+	public static void recTriangle(){
+		theStack = new StackX(100);
+		codePart = 1;
+		while(step() == false){
+			;
+		}
+	}
+	
+	public static boolean step(){
+		
+		switch (codePart) {
+		case 1:
+			theseParams = new Params(theNumber, 6);
+			theStack.push(theseParams);
+			codePart = 2;
+			break;
+		
+		case 2:
+			theseParams = theStack.peek();
+			if(theseParams.n == 1){
+				theAnswer = 1;
+				codePart = 5;
+			}else{
+				codePart = 3;
+			}
+			break;
+			
+		case 3:
+			Params newParams = new Params(theseParams.n -1, 4);
+			theStack.push(newParams);
+			codePart = 2;
+			break;
+			
+		case 4:
+			theseParams = theStack.peek();
+			theAnswer = theAnswer + theseParams.n;
+			codePart = 5;			
+			break;
+		
+		case 5:
+			theseParams = theStack.peek();
+			codePart = theseParams.codePart;
+			theStack.pop();
+			break;
+		
+		case 6:
+			return true;
+			
+		default:
+			break;
+		}
+		
+		return false;
 	}
 
 }
