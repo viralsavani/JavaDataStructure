71413dff80b24ec8ac6eb0900e00986792f29fe7
Viral Savani <viralsavani@gmail.com>
2015-02-01 14:26:33 -0800

    Array partitioning as a part of quick sort

---
+++ src/com/DataStructure/QuickSort/ArrayPartitioning.java
@@ -0,0 +1,79 @@
+package com.DataStructure.QuickSort;
+
+class ArrayImplementation{
+	private double[] theArray; 
+	private int nElems;
+	
+	public ArrayImplementation(int max){
+		theArray = new double[max];
+		nElems = 0;
+	}
+	
+	public void insert(double value){
+		theArray[nElems] = value;
+		nElems++;
+	}
+	
+	public int size(){ 
+		return nElems; 
+	}
+	
+	public void display(){
+		System.out.print("A=");
+		for(int j=0; j<nElems; j++){
+			System.out.print((int)theArray[j] + " ");
+		}
+		System.out.println("");
+	}
+	
+	public int partitionIt(int left, int right, double pivot){
+		
+		int leftPtr = left-1;
+		int rightPtr = right + 1;
+		
+		while(true){
+			while(leftPtr < right && theArray[++leftPtr] < pivot){
+				;	// No Op
+			}
+			
+			while(rightPtr > left && theArray[--rightPtr] > pivot){
+				;	// No Op
+			}
+			
+			if(leftPtr >= rightPtr){
+				break;
+			}else{
+				// Swap Positions
+				double temp = theArray[leftPtr];
+				theArray[leftPtr] = theArray[rightPtr];
+				theArray[rightPtr] = temp;
+			}	
+		}		
+		return leftPtr;
+	}
+}
+
+public class ArrayPartitioning {
+
+	public static void main(String[] args) {
+		int maxSize = 16; // array size
+		ArrayImplementation array = new ArrayImplementation(maxSize);
+		
+		for(int j=0; j<maxSize; j++){ 
+			double n = (int)(java.lang.Math.random()*199);
+			array.insert(n);
+		}
+		
+		array.display();
+		
+		double pivot = 99;
+		System.out.print("Pivot is " + pivot);
+		
+		int size = array.size();
+		
+		int partDex = array.partitionIt(0, size-1, pivot);
+		System.out.println(", Partition is at index " + partDex);
+		array.display();
+	}
+
+}
