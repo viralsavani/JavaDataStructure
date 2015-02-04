d572a280ff2ba379367bb70a83e214bff770453c
Viral Savani <viralsavani@gmail.com>
2015-02-04 10:51:27 -0800

    Method deleted maxSubArray.

--- src/com/Algorithms/MaxSubArray.java
+++ src/com/Algorithms/MaxSubArray.java
@@ -6,13 +6,6 @@ public class MaxSubArray {
 		// TODO Auto-generated method stub
 		int[] inputArray = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
 		
-//		int[] returnArray = maxSubArray(inputArray, 0, inputArray.length-1);
-//		
-//		for (int i = 0; i < returnArray.length; i++) {
-//			int j = returnArray[i];
-//			System.out.println(j);
-//		}
-		
 		System.out.println(FindMaxSumSubArray(inputArray, 0, inputArray.length-1));
 	}
 	
@@ -59,48 +52,7 @@ public class MaxSubArray {
 		return null;
 	}
 	
-	private static int[] maxSubArray(int[] array, int low, int high){
-		
-		int mid = array.length / 2;
-		
-		if(array.length == 1){
-			return array;
-		}else{
-			/** Array notations.
-			 * int[] array = [array-low, array-high, array-sum]
-			 * int[] leftArray = [leftArray-low, leftArray-high, leftArray-sum]
-			 * Same for rest of the arrays.
-			 */
-			
-			int[] leftArray = new int[20]; 
-			leftArray = maxSubArray(array, 0, mid);
-			
-			int[] rightArray = new int[20]; 
-			rightArray = maxSubArray(array, mid+1, array.length-1);
-			
-			int[] crossArray = new int[20]; 
-			rightArray = maxCrossingSubArray(array, low, mid, high);
-			
-			/**
-			 * if (left-sum >= right-sum && left-sum >= cross-sum)
-			 * */
-			if(leftArray[2] >= rightArray[2] && leftArray[2] >= crossArray[2]){
-				return leftArray;
-			}
-			
-			/**
-			 * if (right-sum >= left-sum && right-sum >= cross-sum)
-			 * */
-			
-			else if(rightArray[2] >= leftArray[2] && rightArray[2] >= crossArray[2]){
-				return rightArray;
-			}else{
-				return crossArray;
-			}
-			
-		}
-	}
-	public static int FindMaxSumSubArray(int[] array, int low, int high){
+		public static int FindMaxSumSubArray(int[] array, int low, int high){
 
 		/* No element in the array */
 		if (low > high)  
