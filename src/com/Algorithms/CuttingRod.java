dda8acfcba94b3e4e702a6f0165feae2e5561059
Viral Savani <viralsavani@gmail.com>
2015-03-12 14:35:31 -0700

    Cutting Rod problem with dynamic programming. Theta(n^2).

--- src/com/Algorithms/CuttingRod.java
+++ src/com/Algorithms/CuttingRod.java
@@ -4,12 +4,27 @@ package com.Algorithms;
  * Created by VIRAL on 3/12/2015.
  */
 public class CuttingRod {
+
+    public static long startTime = 0;
+    public static long endTime = 0;
+
     public static void main(String[] args) {
 
-        int[] priceArray = {0,1,5,8,9,10,17,17,20};
-        int rodLength = 4;
+        int[] priceArray = {0,3,5,10,12,14};
+        int rodLength = 5;
 
-        System.out.println(cuttingRodRecursive(priceArray,rodLength));
+        System.out.print("Using Recursion :: ");
+        startTime = System.nanoTime();
+        System.out.print(cuttingRodRecursive(priceArray, rodLength));
+        endTime = System.nanoTime();
+        System.out.println("\t Time taken(nanosecond) :: " + (endTime-startTime) );
+
+
+        System.out.print("Using Bottom Up :: ");
+        startTime = System.nanoTime();
+        System.out.print(cuttingRodBottomUp(priceArray, rodLength));
+        endTime = System.nanoTime();
+        System.out.println("\t Time taken(nanosecond) :: " + (endTime-startTime) );
 
     }
 
@@ -23,5 +38,19 @@ public class CuttingRod {
             }
         }
         return maxFound;
+    }
+
+    public static int cuttingRodBottomUp(int[] price, int piece){
+        int[] revenueArray = new int[piece+1];
+        revenueArray[0] = 0;
+
+        for (int i = 1; i <= piece; i++){
+            int maxFound = Integer.MIN_VALUE;
+            for (int j = 1; j <= i; j++){
+                maxFound = Math.max(maxFound, price[j]+revenueArray[i-j]);
+            }
+            revenueArray[i] = maxFound;
+        }
+        return revenueArray[piece];
     }
 }
