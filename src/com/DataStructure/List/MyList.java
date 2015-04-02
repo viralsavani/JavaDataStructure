740d68469f578295a64bc9f2a02ac7d19696b5b1
Viral Savani <viralsavani@gmail.com>
2015-04-02 12:51:06 -0700

    MyList: List implementation

---
+++ src/com/DataStructure/List/MyList.java
@@ -0,0 +1,43 @@
+package com.DataStructure.List;
+
+import java.util.Arrays;
+
+/**
+ * Created by VIRAL on 4/2/2015.
+ */
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 11;
+    private Object elements[];
+
+    public MyList(){
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int initialSize){
+        elements = new Object[initialSize];
+    }
+
+    public void add(E e){
+        if(size == elements.length){
+            ensureCapacity();
+        }
+        elements[size++] = e;
+    }
+
+    private void ensureCapacity(){
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements,newSize);
+    }
+
+    public E get(int i){
+        if(i >= size || i < 0){
+            throw new IndexOutOfBoundsException("Index:: "+i+" out of range");
+        }
+        return (E) elements[i];
+    }
+
+    public int size(){
+        return (size-1);
+    }
+}
